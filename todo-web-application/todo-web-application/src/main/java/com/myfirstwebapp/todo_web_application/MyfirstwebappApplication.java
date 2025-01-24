5b8938e842c4eaa841573116c3de575539fd47f4
ARU0007 <aruneshagrahari95@gmail.com>
2025-01-24 20:10:12 +0530

    name changed to MyfirstwebappApplication.java

--- todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/MyfirstwebappApplication.java
+++ todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/MyfirstwebappApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoWebApplication {
+public class MyfirstwebappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoWebApplication.class, args);
+		SpringApplication.run(MyfirstwebappApplication.class, args);
 	}
 
 }
