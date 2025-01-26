543bd2dd92bcf5f1afa9c18c9e0721b18b132642
ARU0007 <aruneshagrahari95@gmail.com>
2025-01-26 13:38:34 +0530

    designed to use only get method

--- todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/login/LoginController.java
+++ todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/login/LoginController.java
@@ -2,13 +2,14 @@ package com.myfirstwebapp.todo_web_application.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
 
 
-    @RequestMapping("login")
+    @RequestMapping(value = "login", method = RequestMethod.GET)
     public String gotoLoginPage() {
 
         return "login";
