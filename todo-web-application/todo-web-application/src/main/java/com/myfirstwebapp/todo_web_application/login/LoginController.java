a50c29e824d378115174ec3b46f13034a78420fc
ARU0007 <aruneshagrahari95@gmail.com>
2025-01-26 11:59:49 +0530

    requestparam & Model used

--- todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/login/LoginController.java
+++ todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/login/LoginController.java
@@ -1,13 +1,19 @@
 package com.myfirstwebapp.todo_web_application.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+
+    // http://localhost:8080/login?name=John
+    //req param and model
     @RequestMapping("login")
-    public String gotoLoginPage() {
+    public String gotoLoginPage(@RequestParam String name , ModelMap model) {
+        model.put("name",name);
         return "login";
 
     }
