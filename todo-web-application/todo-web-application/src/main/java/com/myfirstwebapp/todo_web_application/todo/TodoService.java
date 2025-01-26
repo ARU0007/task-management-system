98e4c3f75e4eb330adb129b1f3bdc2a9474d88ee
ARU0007 <aruneshagrahari95@gmail.com>
2025-01-26 20:03:47 +0530

    List of todos created

---
+++ todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/todo/TodoService.java
@@ -0,0 +1,16 @@
+package com.myfirstwebapp.todo_web_application.todo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TodoService {
+    private static List<Todo> todos;
+    static{
+        todos.add(new Todo(1,"Arunesh","learn Spring MVC",
+                LocalDate.now().plusYears(1),false));
+        todos.add(new Todo(2,"Arunesh","learn Spring Security",
+                LocalDate.now().plusYears(2),false));
+        todos.add(new Todo(3,"Arunesh","learn Spring Boot",
+                LocalDate.now().plusYears(3),false));
+    }
+}
