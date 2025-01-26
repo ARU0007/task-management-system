58e2f78cd3760a3d55c3c89db4d40f4385d3a3b7
ARU0007 <aruneshagrahari95@gmail.com>
2025-01-26 19:28:31 +0530

    added @Service annotation

--- todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/login/AuthenticationService.java
+++ todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/login/AuthenticationService.java
@@ -1,5 +1,8 @@
 package com.myfirstwebapp.todo_web_application.login;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthenticationService {
     public boolean authentication(String username, String password){
         boolean isUserAuthenticated =username.equalsIgnoreCase("in28minutes");
