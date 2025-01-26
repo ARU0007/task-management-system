662bb44110a3a2becd7c8cc6033fd5ffb53ecd2a
ARU0007 <aruneshagrahari95@gmail.com>
2025-01-26 19:03:43 +0530

    AuthenticationService.java complited

--- todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/login/AuthenticationService.java
+++ todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/login/AuthenticationService.java
@@ -1,4 +1,9 @@
 package com.myfirstwebapp.todo_web_application.login;
 
 public class AuthenticationService {
+    public boolean authentication(String username, String password){
+        boolean isUserAuthenticated =username.equalsIgnoreCase("in28minutes");
+        boolean isPasswordAuthenticated =password.equalsIgnoreCase("dummy");
+        return isUserAuthenticated && isPasswordAuthenticated;
+    }
 }
