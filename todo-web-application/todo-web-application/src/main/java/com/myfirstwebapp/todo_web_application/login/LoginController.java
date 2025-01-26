b019e510801c25a7d945cf72c72a1c83abdb550c
ARU0007 <aruneshagrahari95@gmail.com>
2025-01-26 13:09:34 +0530

    commented the param

--- todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/login/LoginController.java
+++ todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/login/LoginController.java
@@ -1,20 +1,23 @@
 package com.myfirstwebapp.todo_web_application.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
 
-    // http://localhost:8080/login?name=John
-    //req param and model
+
     @RequestMapping("login")
-    public String gotoLoginPage(@RequestParam String name , ModelMap model) {
-        model.put("name",name);
+    public String gotoLoginPage() {
+
         return "login";
 
     }
 }
+// http://localhost:8080/login?name=John
+//req param and model
+//    @RequestMapping("login")
+//    public String gotoLoginPage(@RequestParam String name , ModelMap model) {
+//        model.put("name",name);
+//        return "login";
