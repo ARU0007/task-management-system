2e58648448501e6abf79a8e7c7f06e8428c5eeb8
ARU0007 <aruneshagrahari95@gmail.com>
2025-02-02 16:55:32 +0530

    created method to softcore the username

--- todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/login/WelcomeController.java
+++ todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/login/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.myfirstwebapp.todo_web_application.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,33 +11,16 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @Controller
 @SessionAttributes("name")
 public class WelcomeController {
-// No need now
-//    public LoginController(AuthenticationService authenticationService) {
-//        super();
-//        this.authenticationService = authenticationService;
-//    }
 
-//    private AuthenticationService authenticationService;
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String gotoWelcomePage(ModelMap model) {
-        model.put("name", "in28minutes");
+        model.put("name",getLoggedinUsername());
         return "welcome";
     }
+    private String getLoggedinUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
+    }
 
-//    @RequestMapping(value = "login", method = RequestMethod.POST)
-//    public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
-//        if (authenticationService.authentication(name, password)) {
-//            model.put("name", name);
-//            model.put("password", password);
-//            return "welcome";
-//        }
-//        model.put("errorMessage","Incalid Credentials! Please try again.");
-//        return "login";
-//    }
+
 }
-// http://localhost:8080/login?name=John
-//req param and model
-//    @RequestMapping("login")
-//    public String gotoLoginPage(@RequestParam String name , ModelMap model) {
-//        model.put("name",name);
-//        return "login";
