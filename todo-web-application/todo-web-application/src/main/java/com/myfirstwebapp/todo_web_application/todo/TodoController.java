58b0d13df1a5dd3539737409541a5c48b33f1518
ARU0007 <aruneshagrahari95@gmail.com>
2025-02-03 00:03:09 +0530

    disabled the TodoController.java

--- todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/todo/TodoController.java
+++ todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/todo/TodoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@Controller
+//@Controller
 @SessionAttributes("name")
 public class TodoController {
 
