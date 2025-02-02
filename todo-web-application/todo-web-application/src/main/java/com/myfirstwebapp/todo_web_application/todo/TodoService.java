e22da6e690f30248ae9234e3bbcf835dc9dc8ab6
ARU0007 <aruneshagrahari95@gmail.com>
2025-02-02 16:55:46 +0530

    added predicate

--- todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/todo/TodoService.java
+++ todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/todo/TodoService.java
@@ -6,28 +6,33 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+
 @Service
 public class TodoService {
-    private static List<Todo> todos = new ArrayList<>();
+    private static final List<Todo> todos = new ArrayList<>();
     private static int todosCount = 0;
 
-    static{
-        todos.add(new Todo(++todosCount,"Arunesh","learn Spring MVC",
-                LocalDate.now().plusYears(1),false));
-        todos.add(new Todo(++todosCount,"Arunesh","learn Spring Security",
-                LocalDate.now().plusYears(2),false));
-        todos.add(new Todo(++todosCount,"Arunesh","learn Spring Boot",
-                LocalDate.now().plusYears(3),false));
+    static {
+        todos.add(new Todo(++todosCount, "Arunesh", "learn Spring MVC", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(++todosCount, "Arunesh", "learn Spring Security", LocalDate.now().plusYears(2), false));
+        todos.add(new Todo(++todosCount, "Arunesh", "learn Spring Boot", LocalDate.now().plusYears(3), false));
     }
-    List<Todo> findByUsername(String username){
-        return todos;
+
+    List<Todo> findByUsername(String username) {
+        Predicate<? super Todo> predicate = todo -> todo.getUsername().equalsIgnoreCase(username);
+
+        return todos.stream().filter(predicate).toList();
     }
+
     public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
-        Todo todo =new Todo(++todosCount,username,description,targetDate,done);
+        Todo todo = new Todo(++todosCount, username, description, targetDate, done);
         todos.add(todo);
     }
+
     public void deleteById(int id) {
-        todos.removeIf(todo -> todo.getId() == id);
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
     }
 
     public Todo findById(int id) {
