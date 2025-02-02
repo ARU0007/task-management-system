e5ea5f059173f906691abd497c46480e9bbf21c1
ARU0007 <aruneshagrahari95@gmail.com>
2025-02-02 23:06:24 +0530

    created some data

---
+++ todo-web-application/todo-web-application/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'ranga', 'Learn JPA', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'ranga', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'ranga', 'Learn Spring Data JPA', CURRENT_DATE(), false);
+
