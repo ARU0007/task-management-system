57aa71b4ee6a58ece6c95e6caf1bfd065b8134b3
ARU0007 <aruneshagrahari95@gmail.com>
2025-02-03 00:03:45 +0530

    findByUsername method added

--- todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/todo/TodoRepository.java
+++ todo-web-application/todo-web-application/src/main/java/com/myfirstwebapp/todo_web_application/todo/TodoRepository.java
@@ -2,5 +2,8 @@ package com.myfirstwebapp.todo_web_application.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
+    public List<Todo> findByUsername(String username);
 }
