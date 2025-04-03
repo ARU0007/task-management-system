86c3943b00ee962dca6efdce773cdd51d791f44d
ARU0007 <aruneshagrahari95@gmail.com>
2025-04-03 12:57:58 +0530

    added todos to harsh ojha

--- todo-web-application/todo-web-application/src/main/resources/data.sql
+++ todo-web-application/todo-web-application/src/main/resources/data.sql
@@ -9,3 +9,12 @@ values(10003, 'in28minutes', 'Learn Spring Data JPA', CURRENT_DATE(), false);
 
 insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
 values(10004, 'arunesh', 'Learn Spring Data JPA', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10005, 'arunesh', 'Learn JPA', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10006, 'arunesh', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10007, 'harsh ojha', 'Marks Assignment', CURRENT_DATE(), false);
